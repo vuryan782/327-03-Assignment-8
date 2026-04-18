3751c2e25dff9b85e390456bf224c15b8e0b6bc5
brandon-smsn <151482300+brandon-smsn@users.noreply.github.com>
2026-04-17 18:03:57 -0700

    tested database connection and added gitignore

--- db.py
+++ db.py
@@ -1,6 +1,11 @@
 import psycopg2
+import os
+from dotenv import load_dotenv
 
-DATABASE_URL = //YOUR_CONNECTION_LINK
+load_dotenv(".env.local") 
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+
 def connect_db():
     return psycopg2.connect(DATABASE_URL)
 
